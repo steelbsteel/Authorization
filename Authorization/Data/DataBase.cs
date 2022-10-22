469e2b66df29a8a1c605a07e39b69261589eb482
steelbsteel <mrtrecer@bk.ru>
2022-10-22 11:10:35 +0300

    Добавьте файлы проекта.

---
+++ Authorization/Data/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace Authorization.Data
+{
+    public class DataBase
+    {
+        public DataBase() { }
+        public void AddToDataBase(User user)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Authorization");
+            var collection = database.GetCollection<User>("UsersCollection");
+            collection.InsertOne(user);
+        }
+    }
+}
---
+++ Authorization/Data/User.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Authorization.Data
+{
+    public class User
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId _id;
+        [BsonIgnoreIfNull]
+        public string login { get; set; }
+        [BsonIgnoreIfNull]
+        public string password { get; set; }
+        [BsonIgnoreIfNull]
+        public string name { get; set; }
+        [BsonIgnoreIfNull]
+        public string surname { get; set; }
+        [BsonIgnoreIfNull]
+        public string email { get; set; }
+
+        /*
+        public User(string newLogin, string newPassword, string newName, string newSurname, string newEmail)
+        {
+            login = newLogin;
+            password = newPassword;
+            name = newName;
+            surname = newSurname;
+            email = newEmail;
+        }
+        */
+    }
+}
